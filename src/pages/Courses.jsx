a7c76c88fe4b7a85969e6556d9caed721b3a208b
Cedar Daniel <cedardaniel1998@gmail.com>
2023-12-08 18:24:03 +0100

    fixed build error

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,45 +1,43 @@
-import React from "react";
-import MainLayout from "./MainLayout";
-import logo from "../assets/svg/logo.svg";
-import bell from "../assets/svg/bell.svg";
-import Button from "../components/elements/Button";
-import Stack from "../components/coursescomponents/Stack";
-import MyCourses from "../components/coursescomponents/MyCourses";
-import ClassNotification from "../components/coursescomponents/classnotification";
-import UpcomingClasses from "../components/coursescomponents/UpcomingClasses";
+import React from 'react';
+import MainLayout from './MainLayout';
+import logo from '../assets/svg/logo.svg';
+import bell from '../assets/svg/bell.svg';
+import Button from '../components/elements/Button';
+import Stack from '../components/coursescomponents/Stack';
+import MyCourses from '../components/coursescomponents/MyCourses';
+import ClassNotification from '../components/coursescomponents/ClassNotification';
+import UpcomingClasses from '../components/coursescomponents/UpcomingClasses';
 
 const Courses = () => {
   return (
-    
-      <div className=" px-5">
-        
-        <section className="mt-10">
-          <div className="section-head">
-          
-            <h2 className="text-2xl font-medium">Add New Course</h2>
-            <p className="text-xs mt-2 text-textpink">
-              It is a long established fact that a reader will be distracted by
-              the readable <br /> content of a page when looking at its layout
-            </p>
-          </div>
+    <div className=" px-5">
+      <section className="mt-10">
+        <div className="section-head">
+          <h2 className="text-2xl font-medium">Add New Course</h2>
+          <p className="text-xs mt-2 text-textpink">
+            It is a long established fact that a reader will be distracted by
+            the readable <br /> content of a page when looking at its layout
+          </p>
+        </div>
 
-          <h3 className=" text-[1.25rem] mt-[1.63rem] font-medium">Select Stack</h3>
-         
-          <Stack />
-          
-          <div className="mt-24">
-            <h1 className="text-2xl font-medium">My Courses(2)</h1>
-            <MyCourses />
-          </div>
+        <h3 className=" text-[1.25rem] mt-[1.63rem] font-medium">
+          Select Stack
+        </h3>
 
-          <div className="flex gap-x-44 items-center flex-wrap mb-24">
-            <ClassNotification />
-            <UpcomingClasses />
-          </div>
-          <p className="load"></p>
-        </section>
-      </div>
-    
+        <Stack />
+
+        <div className="mt-24">
+          <h1 className="text-2xl font-medium">My Courses(2)</h1>
+          <MyCourses />
+        </div>
+
+        <div className="flex gap-x-44 items-center flex-wrap mb-24">
+          <ClassNotification />
+          <UpcomingClasses />
+        </div>
+        <p className="load"></p>
+      </section>
+    </div>
   );
 };
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "../pages/Signup";
-import Login from "../pages/Login";
-import Dashboard from "../pages/Dashboard";
-import Settings from "../pages/Settings";
-import Courses from "../pages/Courses";
-import MainLayout from "../pages/MainLayout";
-import Lessons from "../pages/Lessons";
-import LessonList from "../pages/LessonList";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from '../pages/Signup';
+import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+import Settings from '../pages/Settings';
+import Courses from '../pages/Courses';
+import MainLayout from '../pages/MainLayout';
+import Lessons from '../pages/Lessons';
+import LessonList from '../pages/LessonList';
 
 const AppRouter = () => {
   return (
@@ -15,13 +15,16 @@ const AppRouter = () => {
       <Routes>
         <Route index path={`/`} element={<Login />} />
         <Route path={`/signup`} element={<Signup />} />
-        <Route path={`/lms`} element={<MainLayout   />}>
-        <Route path={`/lms/dashboard`} element={<Dashboard />} />
-        <Route path={`/lms/courses`} element={<Courses />} />
-      <Route path={`/lms/lessons`} element={<Lessons defaultTitle={"Lessons"} />}>
-        <Route path={`/lms/lessons/:list`} element={<LessonList />} />
-      </Route>
-        <Route path={`/lms/settings`} element={<Settings />} />
+        <Route path={`/lms`} element={<MainLayout />}>
+          <Route path={`/lms/dashboard`} element={<Dashboard />} />
+          <Route path={`/lms/courses`} element={<Courses />} />
+          <Route
+            path={`/lms/lessons`}
+            element={<Lessons defaultTitle={'Lessons'} />}
+          >
+            <Route path={`/lms/lessons/:list`} element={<LessonList />} />
+          </Route>
+          <Route path={`/lms/settings`} element={<Settings />} />
         </Route>
       </Routes>
     </BrowserRouter>
