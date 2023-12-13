fe1956f5f2780aa446eff6247653486a48698692
Cedar Daniel <cedardaniel1998@gmail.com>
2023-12-13 13:08:42 +0100

    cleared consoole

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import Layout from './Layout';
 import Button from '../components/elements/Button';
-import {faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Input from '../components/elements/Input';
 import { Link, useNavigate } from 'react-router-dom';
-import axios from "axios"
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -27,41 +26,29 @@ const Login = () => {
     });
   };
 
-  const handleLogin =  async (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
     const data = {
       email: formData.email,
       password: formData.password,
-     
     };
-    const url= import.meta.env.VITE_APP_BASE_URL;
-    
+    const url = import.meta.env.VITE_APP_BASE_URL;
 
     try {
       setIsLoading(true);
       const response = await axios.post(`${url}/api/v1/users/login`, data);
 
-    if (response.status === 200) {
-      toast.success("Login successful!");
-      localStorage.setItem("userData",  JSON.stringify(response.data));
-      setIsLoading(false);
-      navigate('/lms/dashboard');
-
-    } else {
-      
-      toast.error("Login failed.");
+      if (response.status === 200) {
+        toast.success('Login successful!');
+        localStorage.setItem('userData', JSON.stringify(response.data));
+        setIsLoading(false);
+        navigate('/lms/dashboard');
+      } else {
+        toast.error('Login failed.');
+      }
+    } catch (error) {
+      toast.error('login failed. Please try again.');
     }
-  } catch (error) {
-      toast.error("login failed. Please try again.");
-  }
-
-
-
-
-    
-    console.log('Form Data:', formData);
-   
-    
   };
 
   const togglePasswordVisibility = () => {
@@ -70,16 +57,17 @@ const Login = () => {
 
   return (
     <Layout>
-
-
       <div className="Welcome text-center mt-10">
-          <p className="text-2xl font-semibold mb-2">Welcome Back!</p>
-          <p>Welcome back! Please enter your details.</p>
-        </div>
+        <p className="text-2xl font-semibold mb-2">Welcome Back!</p>
+        <p>Welcome back! Please enter your details.</p>
+      </div>
 
-        <form className="mt-10 grid gap-6 lg:w-full " id="login-form" onSubmit={handleLogin}>
-
-        <Input 
+      <form
+        className="mt-10 grid gap-6 lg:w-full "
+        id="login-form"
+        onSubmit={handleLogin}
+      >
+        <Input
           label="Email Address"
           type="email"
           name="email"
@@ -88,52 +76,60 @@ const Login = () => {
           required
           value={formData.email}
           onChange={handleInputChange}
-         
-
-        
         />
-         
 
-          <div>
-            <p className="inputname">Password</p>
-            <div className="flex items-center w-full p-3 rounded-lg border-2 border-[#E0E0E0]" id="login-password">
-              <input
-                type={showPassword ? 'text' : 'password'}
-                placeholder="********************"
-                name="password"
-                required
-                value={formData.password}
-                onChange={handleInputChange}
-                className="w-full bg-transparent outline-none"
-              />
-              <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash } className="eyeicon" onClick={togglePasswordVisibility} />
-            </div>
+        <div>
+          <p className="inputname">Password</p>
+          <div
+            className="flex items-center w-full p-3 rounded-lg border-2 border-[#E0E0E0]"
+            id="login-password"
+          >
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder="********************"
+              name="password"
+              required
+              value={formData.password}
+              onChange={handleInputChange}
+              className="w-full bg-transparent outline-none"
+            />
+            <FontAwesomeIcon
+              icon={showPassword ? faEye : faEyeSlash}
+              className="eyeicon"
+              onClick={togglePasswordVisibility}
+            />
           </div>
+        </div>
 
-          <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center">
           <Input
             inputClass={'flex gap-2 items-center flex-row-reverse'}
-              label="Remember me"
+            label="Remember me"
             labelClass=" w-full flex-1"
-              type="checkbox"
-              id="checkbox"
-              name="checkbox"
-              className="w-4 h-4"
-              required
-            />
-            <p><i>Forgot Password?</i></p>
-          </div>
+            type="checkbox"
+            id="checkbox"
+            name="checkbox"
+            className="w-4 h-4"
+            required
+          />
+          <p>
+            <i>Forgot Password?</i>
+          </p>
+        </div>
 
-          <div className="flex flex-col justify-center items-center">
-            <Button type="submit" className={''}>{isLoading ? 'Login....' : 'Login'}</Button>
-            <p className=' mt-6 text-[#E0E0E0]'>
-              Already have an account? <Link to="/signup"><em className=' text-tertiary_blue'>Sign Up</em></Link>
-            </p>
-          </div>
-        </form>
-     
+        <div className="flex flex-col justify-center items-center">
+          <Button type="submit" className={''}>
+            {isLoading ? 'Login....' : 'Login'}
+          </Button>
+          <p className=" mt-6 text-[#E0E0E0]">
+            Already have an account?{' '}
+            <Link to="/signup">
+              <em className=" text-tertiary_blue">Sign Up</em>
+            </Link>
+          </p>
+        </div>
+      </form>
     </Layout>
-   
   );
 };
 
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,109 +1,108 @@
-import React, { useEffect, useState } from "react";
-import MainLayout from "./MainLayout";
-import logo from "../assets/svg/logo.svg";
-import bell from "../assets/svg/bell.svg";
-import Input from "../components/elements/Input";
-import Button from "../components/elements/Button";
+import React, { useEffect, useState } from 'react';
+import MainLayout from './MainLayout';
+import logo from '../assets/svg/logo.svg';
+import bell from '../assets/svg/bell.svg';
+import Input from '../components/elements/Input';
+import Button from '../components/elements/Button';
 
 const Settings = () => {
-  const [userInitials, setUserInitials] = useState("");
-  const [firtName, setfirtName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  console.log(userInitials);
-  console.log(firtName);
-  console.log(lastName);
-  console.log(email);
-
+  const [userInitials, setUserInitials] = useState('');
+  const [firtName, setfirtName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
 
   useEffect(() => {
-      
-    const userData = JSON.parse(localStorage.getItem("userData"));
+    const userData = JSON.parse(localStorage.getItem('userData'));
     const user = userData?.user;
-    console.log('userData', user);
-  
+
     if (userData) {
-      
       const { firstName, lastName, email } = user;
       setfirtName(firstName);
       setLastName(lastName);
       setEmail(email);
-  
 
-      const firstNameInitial = firstName ? firstName.charAt(0).toUpperCase() : "";
-      const lastNameInitial = lastName ? lastName.charAt(0).toUpperCase() : "";
-  
-     
+      const firstNameInitial = firstName
+        ? firstName.charAt(0).toUpperCase()
+        : '';
+      const lastNameInitial = lastName ? lastName.charAt(0).toUpperCase() : '';
+
       const initials = `${firstNameInitial}${lastNameInitial}`;
       setUserInitials(initials);
     }
   }, []);
   return (
-    
-      <div className="mb-[10.56rem] xl:px-12">
-        
-        <div className="flex items-center gap-4 w-fit mx-5 mt-16">
-          <h1 className=" font-bold border-[4px] border-[#0756CD] p-[0.39rem] w-fit rounded-full mx-auto">
-            {userInitials}
+    <div className="mb-[10.56rem] xl:px-12">
+      <div className="flex items-center gap-4 w-fit mx-5 mt-16">
+        <h1 className=" font-bold border-[4px] border-[#0756CD] p-[0.39rem] w-fit rounded-full mx-auto">
+          {userInitials}
+        </h1>
+        <div>
+          <h1 className="text-2xl font-semibold ">
+            {firtName} {lastName}
           </h1>
-          <div>
-            <h1 className="text-2xl font-semibold ">{firtName} {lastName}</h1>
-            <p>{email}</p>
-          </div>
+          <p>{email}</p>
         </div>
-        <section className="mx-5 lg:flex justify-between xl:gap-20 lg:gap-10">
-          <div className="lg:w-full">
-            <h2 className=" text-2xl font-semibold mt-12 mb-6">Personal Information</h2>
-            <form action="" >
-              <Input
-                label="First Name"
-                labelClass={``}
-                className="w-full mt-2"
-                type="text"
-                name="firstname"
-              />
-              <Input
-                label="Last Name"
-                labelClass="pb-2"
-                inputClass={`my-6`}
-                className="w-full mt-2"
-                type="text"
-                name="lastname"
-              />
-              <Input
-                label="phone"
-                inputClass={`mb-[2.25rem]`}
-                labelClass="pb-2"
-                className="w-full mt-2"
-                type="tel"
-                name="phone"
-              />
-              <Button type={"submit"} className={"block mx-auto px-[5.5rem]"}>Save Changes</Button>
-            </form>
-          </div>
-          <div className=" lg:w-full">
-            <h2 className=" text-2xl font-semibold mt-12 mb-6">Change Password</h2>
-            <form action="">
-              <Input
-                label="New Password"
-                labelClass="text-lg"
-                className="w-full mt-2"
-                type="password"
-                name="newpassword"
-              />
-              <Input
-                label="Confirm Password"
-                inputClass={`mb-[2.25rem] mt-6`}
-                className="w-full mt-2"
-                type="password"
-                name="confirmpassword"
-              />
-              <Button type={"submit"} className={"block mx-auto px-[4.5rem]"}>Change Password</Button>
-            </form>
-          </div>
-        </section>
       </div>
-    
+      <section className="mx-5 lg:flex justify-between xl:gap-20 lg:gap-10">
+        <div className="lg:w-full">
+          <h2 className=" text-2xl font-semibold mt-12 mb-6">
+            Personal Information
+          </h2>
+          <form action="">
+            <Input
+              label="First Name"
+              labelClass={``}
+              className="w-full mt-2"
+              type="text"
+              name="firstname"
+            />
+            <Input
+              label="Last Name"
+              labelClass="pb-2"
+              inputClass={`my-6`}
+              className="w-full mt-2"
+              type="text"
+              name="lastname"
+            />
+            <Input
+              label="phone"
+              inputClass={`mb-[2.25rem]`}
+              labelClass="pb-2"
+              className="w-full mt-2"
+              type="tel"
+              name="phone"
+            />
+            <Button type={'submit'} className={'block mx-auto px-[5.5rem]'}>
+              Save Changes
+            </Button>
+          </form>
+        </div>
+        <div className=" lg:w-full">
+          <h2 className=" text-2xl font-semibold mt-12 mb-6">
+            Change Password
+          </h2>
+          <form action="">
+            <Input
+              label="New Password"
+              labelClass="text-lg"
+              className="w-full mt-2"
+              type="password"
+              name="newpassword"
+            />
+            <Input
+              label="Confirm Password"
+              inputClass={`mb-[2.25rem] mt-6`}
+              className="w-full mt-2"
+              type="password"
+              name="confirmpassword"
+            />
+            <Button type={'submit'} className={'block mx-auto px-[4.5rem]'}>
+              Change Password
+            </Button>
+          </form>
+        </div>
+      </section>
+    </div>
   );
 };
 
